6f68ea967a8399723d43ff800c7572b443a28647
raych2 <rtongco@gmail.com>
2021-03-24 20:37:31 -0700

    Remove error alert and add div to display error message

--- src/weather.js
+++ src/weather.js
@@ -1,17 +1,18 @@
 import { processData, renderDisplay } from './display';
 
+const errorMessage = document.querySelector('.error');
+
 async function fetchWeatherData(location) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=eca8bb460056c04ed47adc55ea4e1a64`,
     { mode: 'cors' }
   );
-  if (response.status === 400) {
-    alert('Enter a city');
-  } else if (response.status === 404) {
-    alert('City not found');
+  if (response.status === 400 || response.status === 404) {
+    errorMessage.innerText = `${location} not found`;
   } else {
     const weatherData = await response.json();
     const weatherObj = processData(weatherData);
+    errorMessage.innerText = '';
     renderDisplay(weatherObj);
   }
 }
